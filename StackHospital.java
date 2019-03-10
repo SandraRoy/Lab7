f75ca2aa6bd84bbae0aff930269c244843bf7924
Sandra Roy <Sandra.D.Roy-1@ou.edu>
2019-03-10 14:48:20 -0500

    return no. of patients

--- StackHospital.java
+++ StackHospital.java
@@ -1,4 +1,22 @@
+import java.util.Stack;
 
-public class StackHospital {
-
-}
+public class StackHospital <PatientType> extends Hospital <PatientType>
+{
+	Stack <PatientType> Patient=new Stack <PatientType>();
+	
+	public void AddPatient(PatientType patient)
+	{
+		Patient.push(patient);
+	}
+	public PatientType nextPatient()
+	{
+		return Patient.peek();
+	}
+	public PatientType treatNextPatient()
+	{
+		 return Patient.pop();
+	}
+	public int numPatients()
+	{
+		return Patient.size();
+	}
